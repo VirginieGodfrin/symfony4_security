b1a30934d2360b042df348159c23fa8c3d9dac70
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-11-07 12:05:11 +0100

    Chapter 15: Deny Access in the Controller

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -7,14 +7,23 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+// third way to deny access controll : Protecting an Entire Controller Class
+/**
+ * @IsGranted("ROLE_ADMIN") 
+ */
 class CommentAdminController extends Controller
 {
+    // second way to deny access controll : IsGranted Annotation
     /**
      * @Route("/admin/comment", name="comment_admin")
+     * @IsGranted("ROLE_USER")
      */
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator)
     {
+        // first easy way to deny access controll
+        // $this->denyAccessUnlessGranted('ROLE_USER');
         $q = $request->query->get('q');
 
         $queryBuilder = $repository->getWithSearchQueryBuilder($q);
