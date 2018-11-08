a4d8338aa5b0558fe29124b83c0a1d06d5015641
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-11-08 16:35:10 +0100

    Chapter 25: ApiToken Entity

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -6,6 +6,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\ApiToken;
 
 // The way you use BaseFixture class is still the same: 
 //  extend BaseFixture and update the load() method to be protected function loadData() . 
@@ -24,7 +25,7 @@ class UserFixture extends BaseFixture
     protected function loadData(ObjectManager $manager)
     {
 
-    	$this->createMany(10, 'main_users', function($i) {
+    	$this->createMany(10, 'main_users', function($i) use ($manager) {
     		$user = new User();
     		$user->setEmail(sprintf('spacebar%d@example.com', $i));
     		// We an use Faker, which we already setup inside
@@ -38,6 +39,11 @@ class UserFixture extends BaseFixture
             if ($this->faker->boolean) { 
                 $user->setTwitterUsername($this->faker->userName);
             }
+            // baseFixture persist the createMany() entity, but not those who are manualy created
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+            $manager->persist($apiToken1); 
+            $manager->persist($apiToken2);
 
     		return $user;
 		});
---
+++ src/Entity/ApiToken.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ApiTokenRepository")
+ */
+class ApiToken
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $expiresAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="apiTokens")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+    // use constructor instead setter
+    // Yep, our token class is now immutable, which wins us major hipster points. 
+    // Immutable just means that, once it's instantiated, this object's data can never be changed.
+    // it makes more sense to setup some things in the constructor and remove the setter methods if you don't need them.
+    public function __construct(User $user){
+        $this->token = bin2hex(random_bytes(60));
+        $this->user = $user;
+        $this->expiresAt = new \DateTime('+8 hour');
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function getExpiresAt(): ?\DateTimeInterface
+    {
+        return $this->expiresAt;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+}
