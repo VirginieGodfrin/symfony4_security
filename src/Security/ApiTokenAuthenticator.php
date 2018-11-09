58a58d581c3d933649820e417acf55c2d385a582
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-11-09 17:06:27 +0100

    Chapter 27: API Token Authenticator

--- src/Security/ApiTokenAuthenticator.php
+++ src/Security/ApiTokenAuthenticator.php
@@ -8,27 +8,51 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use App\Repository\ApiTokenRepository;
 
 class ApiTokenAuthenticator extends AbstractGuardAuthenticator
 {
+    private $apiTokenRepo;
+
+    public function __construct(ApiTokenRepository $apiTokenRepo)
+    {
+        $this->apiTokenRepo = $apiTokenRepo; 
+    }
+
     public function supports(Request $request)
     {
-        // todo
+        // check if request have an Authorization header that start with Bearer
+        return $request->headers->has('Authorization')
+            && 0 === strpos($request->headers->get('Authorization'), 'Bearer ');
+
     }
 
     public function getCredentials(Request $request)
     {
-        // todo
+        // getCredentials: read the token string and return it.
+        $authorizationHeader = $request->headers->get('Authorization');
+        // return token without bearer
+        return substr($authorizationHeader, 7);
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        // dump($credentials);die;
+        // query the ApiToken entity & get the user !
+        $token = $this->apiTokenRepo->findOneBy([ 
+            'token' => $credentials
+        ]);
+
+        if (!$token) { 
+            return;
+        }
+
+        return $token->getUser();
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // todo
+        dd('checking credentials');
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
